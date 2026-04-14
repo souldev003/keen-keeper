ef6bcd9e68f54396f543a7d1e8f42f84843946af
souldev003 <mehrabhosen003@gmail.com>
2026-04-14 21:19:52 +0600

    Update next_due_date values in data.json to 2026 for all friends; enhance error handling in getFriendDetails function and improve FriendDetailsPage layout with additional features

--- src/app/friend/[id]/page.jsx
+++ src/app/friend/[id]/page.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import Image from "next/image";
+import {
+  BsBell,
+  BsArchive,
+  BsTrash,
+  BsTelephone,
+  BsChatLeftText,
+  BsCameraVideo,
+} from "react-icons/bs";
 
 async function getFriendDetails(id) {
-  const res = await fetch(`http://localhost:3000/data.json`, {
-    cache: "no-store",
-  });
+  try {
+    const res = await fetch(`http://localhost:3000/data.json`, {
+      cache: "no-store",
+    });
 
-  const friends = await res.json();
-  // console.log(friends);
+    if (!res.ok) return null;
 
-  const friend = friends.find((friend) => friend.id.toString() === id);
+    const friends = await res.json();
+    const friend = friends.find((friend) => friend.id.toString() === id);
 
-  return friend;
+    return friend;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return null;
+  }
 }
 
 const FriendDetailsPage = async ({ params }) => {
@@ -19,11 +32,156 @@ const FriendDetailsPage = async ({ params }) => {
   const id = getParam.id;
 
   const friend = await getFriendDetails(id);
-  console.log(friend);
+
+  if (!friend) {
+    return <div className="p-4 text-center">Friend not found.</div>;
+  }
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4"> Friend Name: {friend.name} </h1>
+    <div className="max-w-7xl mx-auto p-4 md:p-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="flex flex-col gap-6">
+          <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-100 flex flex-col items-center text-center">
+            <div className="relative w-28 h-28 mb-4">
+              <div className="relative w-full h-full rounded-full overflow-hidden border-2 border-gray-50 shadow-inner">
+                <Image
+                  src={friend.picture}
+                  alt={friend.name}
+                  fill
+                  unoptimized={true}
+                  className="object-cover rounded-full"
+                />
+              </div>
+            </div>
+
+            <h1 className="text-xl font-bold text-gray-900 mb-2">
+              {friend.name}
+            </h1>
+
+            <span
+              className={`px-4 py-1.5 rounded-full text-xs font-bold text-white uppercase mb-3 ${
+                friend.status === "overdue"
+                  ? "bg-[#e65757]"
+                  : friend.status === "on-track"
+                    ? "bg-[#244D3F]"
+                    : friend.status === "almost-due"
+                      ? "bg-[#EFAD44]"
+                      : friend.status === "needs-attention"
+                        ? "bg-[#f80000]"
+                        : friend.status === "active"
+                          ? "bg-[#15dd1f]"
+                          : "bg-gray-400"
+              }`}
+            >
+              {friend.status}
+            </span>
+
+            <div className="flex flex-wrap gap-2 justify-center mb-4">
+              {friend.tags?.map((tag, idx) => (
+                <span
+                  key={idx}
+                  className="px-3 border border-[#79be8f] py-1.5 bg-[#CBFADB] text-[#35493c] text-[11px] font-bold uppercase rounded-full"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+
+            <p className="text-sm text-[#64748B] italic mb-1">{friend.bio}</p>
+            <p className="text-xs text-gray-400 font-mono">
+              Email: {friend.email}
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <button className="text-gray-700 w-full bg-white text-left p-4 rounded-lg shadow-sm border border-gray-100 flex items-center gap-3 text-sm font-semibold hover:bg-gray-50 transition ">
+              <BsBell className="text-lg text-gray-500" />
+              Snooze 2 Weeks
+            </button>
+
+            <button className="text-gray-700 w-full bg-white text-left p-4 rounded-lg shadow-sm border border-gray-100 flex items-center gap-3 text-sm font-semibold hover:bg-gray-50 transition ">
+              <BsArchive className="text-lg text-gray-500" />
+              Archive
+            </button>
+
+            <button className="text-red-600 border-red-100 w-full bg-white text-left p-4 rounded-lg shadow-sm border flex items-center gap-3 text-sm font-semibold hover:bg-red-100 transition ">
+              <BsTrash className="text-lg" />
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div className="md:col-span-2 flex flex-col gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="bg-white p-8 rounded-md shadow-sm border border-gray-100 flex flex-col items-center justify-center text-center">
+              <div className="text-3xl font-extrabold text-gray-900 mb-2">
+                {friend.days_since_contact}
+              </div>
+              <div className="text-sm font-medium text-gray-500">
+                Days Since Contact
+              </div>
+            </div>
+
+            <div className="bg-white p-8 rounded-md shadow-sm border border-gray-100 flex flex-col items-center justify-center text-center">
+              <div className="text-3xl font-extrabold text-gray-900 mb-2">
+                {friend.goal}
+              </div>
+              <div className="text-sm font-medium text-gray-500">
+                Goal (Days)
+              </div>
+            </div>
+
+            <div className="bg-white p-8 rounded-md shadow-sm border border-gray-100 flex flex-col items-center justify-center text-center">
+              <div className="text-2xl font-extrabold text-gray-900 mb-2">
+                {new Date(friend.next_due_date).toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </div>
+              <div className="text-sm font-medium text-gray-500">Next Due</div>
+            </div>
+          </div>
+
+          <div className="bg-white p-8 rounded-md shadow-sm border border-gray-100 flex flex-col gap-6">
+            <div className="flex justify-between items-center">
+              <h2 className="text-lg font-bold text-gray-800">
+                Relationship Goal
+              </h2>
+              <button className="text-sm font-semibold text-gray-600 px-4 py-1.5 rounded-lg border border-gray-200 hover:bg-gray-50">
+                Edit
+              </button>
+            </div>
+
+            <p className="text-gray-600 flex gap-2 items-center text-sm">
+              Connect every{" "}
+              <strong className="text-gray-900 text-base">
+                {friend.goal} days
+              </strong>
+            </p>
+          </div>
+
+          <div className="bg-white p-5 md:p-8 rounded-md shadow-sm border border-gray-100 flex flex-col gap-6">
+            <h2 className="text-lg font-bold text-gray-800">Quick Check-In</h2>
+            <div className="flex gap-4">
+              <button className="flex-1 bg-white p-3 md:p-5 rounded-md border border-gray-100 flex flex-col items-center justify-center gap-2 font-semibold text-gray-800 hover:bg-gray-50 transition cursor-pointer">
+                <BsTelephone className="text-2xl text-gray-700" />
+                Call
+              </button>
+
+              <button className="flex-1 bg-white p-3 md:p-5 rounded-md border border-gray-100 flex flex-col items-center justify-center gap-2 font-semibold text-gray-800 hover:bg-gray-50 transition cursor-pointer">
+                <BsChatLeftText className="text-2xl text-gray-700" />
+                Text
+              </button>
+
+              <button className="flex-1 bg-white p-3 md:p-5 rounded-md border border-gray-100 flex flex-col items-center justify-center gap-2 font-semibold text-gray-800 hover:bg-gray-50 transition cursor-pointer">
+                <BsCameraVideo className="text-2xl text-gray-700" />
+                Video
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
