ab28298181fc064e38e2ac6b531f54c8bbc08dd8
souldev003 <mehrabhosen003@gmail.com>
2026-04-14 13:03:44 +0600

    Add FriendStats component and implement ActiveLink for navigation

--- src/components/Home/FriendStats.jsx
+++ src/components/Home/FriendStats.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 const FriendStats = () => {
   return (
-    <div className="bg-[#F8FAFC] min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold text-[#244D3F]">Friend Stats</h1>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 mt-10">
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+        <span className="text-3xl font-semibold text-[#244D3F]">15</span>
+        <h3 className="text-lg text-[#64748B] mb-2">Total Friends</h3>
+      </div>
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+        <span className="text-3xl font-semibold text-[#244D3F]">15</span>
+        <h3 className="text-lg text-[#64748B] mb-2">On Track</h3>
+      </div>
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+        <span className="text-3xl font-semibold text-[#244D3F]">15</span>
+        <h3 className="text-lg text-[#64748B] mb-2">Need Attention</h3>
+      </div>
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+        <span className="text-3xl font-semibold text-[#244D3F]">15</span>
+        <h3 className="text-lg text-[#64748B] mb-2">Interaction This Month</h3>
+      </div>
     </div>
   );
 };
