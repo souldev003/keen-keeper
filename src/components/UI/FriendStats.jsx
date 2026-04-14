4591f86f5f4d2a6592aed11bc265d47a995d751b
souldev003 <mehrabhosen003@gmail.com>
2026-04-14 15:40:07 +0600

    Update status formatting in data.json and adjust FriendList and FriendStats components for consistency

--- src/components/UI/FriendStats.jsx
+++ src/components/UI/FriendStats.jsx
@@ -3,32 +3,37 @@ import React from "react";
 const FriendStats = ({ friends }) => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pb-10 border-b-2 border-gray-200 mb-10">
-      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md text-center">
         <span className="text-3xl font-semibold text-[#244D3F]">
           {friends.length}
         </span>
         <h3 className="text-lg text-[#64748B] mb-2">Total Friends</h3>
       </div>
-      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md text-center">
         <span className="text-3xl font-semibold text-[#244D3F]">
           {friends.filter((friend) => friend.status === "on-track").length}
         </span>
         <h3 className="text-lg text-[#64748B] mb-2">On Track</h3>
       </div>
-      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
-        <span className="text-3xl font-semibold text-[#244D3F]">
-          {friends.filter((friend) => friend.status === "overdue").length}
-        </span>
-        <h3 className="text-lg text-[#64748B] mb-2">Overdue</h3>
-      </div>
-      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md">
+
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md text-center">
         <span className="text-3xl font-semibold text-[#244D3F]">
           {
             friends.filter((friend) => friend.status === "needs-attention")
               .length
           }
         </span>
-        <h3 className="text-lg text-[#64748B] mb-2">Needs Attention</h3>
+        <h3 className="text-lg text-[#64748B] mb-2">Need Attention</h3>
+      </div>
+
+      <div className="p-8 bg-[#FFFFFF] rounded-md shadow-md text-center">
+        <span className="text-3xl font-semibold text-[#244D3F]">
+          {friends.filter((friend) => friend.days_since_contact <= 30).length}
+        </span>
+        <h3 className="text-lg text-[#64748B] mb-2">
+          Interaction This Months{" "}
+        </h3>
       </div>
     </div>
   );
