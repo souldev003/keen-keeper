45045ce68f2f65203aaaa99eac9c752db48a7a08
souldev003 <mehrabhosen003@gmail.com>
2026-04-16 02:33:19 +0600

    text size promblem fixed

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -5,7 +5,7 @@ const StatsPage = () => {
   return (
     <div className="bg-[#F8FAFC] min-h-screen">
       <div className="max-w-2xl mx-auto my-8 lg:my-20">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">
+        <h1 className="text-2xl p-3 md:text-3xl font-bold text-gray-800 mb-6">
           Friendship Analytics
         </h1>
         <StatsUI />
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -12,7 +12,7 @@ const TimeLinePage = () => {
     <div className="bg-[#F8FAFC] min-h-screen">
       <div className="max-w-2xl mx-auto py-8 lg:pt-20">
         <div>
-          <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold">
+          <h1 className="text-2xl p-3 md:text-3xl lg:text-5xl font-bold">
             Timeline
           </h1>
         </div>
